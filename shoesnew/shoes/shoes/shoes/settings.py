546cd03945c8ddc453a57d596fd0e9413a5141bc
MohammadYahya2 <mohammad1.yahya2019@gmail.com>
2025-07-29 13:17:44 +0300

    Configure URL patterns to serve media file22s

--- shoesnew/shoes/shoes/shoes/settings.py
+++ shoesnew/shoes/shoes/shoes/settings.py
@@ -86,8 +86,16 @@ WSGI_APPLICATION = 'shoes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # محرك قاعدة البيانات
+        'NAME': os.environ.get('DB_NAME'),         # اسم قاعدة البيانات
+        'USER': os.environ.get('DB_USER'),         # اسم المستخدم
+        'PASSWORD': os.environ.get('DB_PASSWORD'), # كلمة المرور
+        'HOST': os.environ.get('DB_HOST'),         # المضيف
+        'PORT': os.environ.get('DB_PORT'),         # المنفذ
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
